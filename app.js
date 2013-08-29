c371890f91cac149f61f699957d3fcfbcf72720c
Calvin Feder <calvinfeder@gmail.com>
2013-08-28 20:02:12 -0700

    Cleaned up code

--- app.js
+++ app.js
@@ -49,8 +49,8 @@ app.get('/resize/:u/:x', function(req, res, next){
 					+ 'Please provide a URL with a .jpg, .jpeg, .bmp, or .png extention.');
 		} else{
 			http.get(options, function(response) {
-				if(response.statusCode != 200) { //if the status code of the response was not 200,
-					//or request got an error, I know that I didn't get the image I expected
+				if(response.statusCode != 200) { //if the status code of the response was not 200
+											//I know that I didn't get the image I expected
 					res.send(415, 'Sorry, the URL provided '
 							+ 'did not respond with an \'OK\' status code');
 				} else{
@@ -73,74 +73,12 @@ app.get('/resize/:u/:x', function(req, res, next){
 						});
 					});
 				}
+			}).on('error', function(e) { //this covers the case of a URL where the DNS entry cannot be found
+				res.send(415, 'Sorry, there was an error with the HTTP get, '
+						+ 'error message: ' + e.message);
 			});
 		}
 	}
-	
-	/*
-	 var tempimg = '';
-	 var tempdest = '';
-	 if(x > 10000 || x <= 0){//only accept dimensions from 1 to 10000 pixels
-		res.send(406, 'Sorry, the dimensions provided were outside the acceptable range.  '
-				+ 'Please provide dimensions from 1 to 10000 pixels.');
-	} else{
-		if (ext != '.jpg' && ext != '.png' && ext != '.jpeg' && ext != '.bmp') {//only accept these file formats
-			res.send(415, 'Sorry, the URL provided is not an accepted image format. ' 
-					+ 'Please provide a URL with a .jpg, .jpeg, .bmp, or .png extention.');
-		} else{
-			temp.open({suffix: '.jpg'}, function(err, info){//Create a temporary file
-				if (err) throw err;
-				tempimg = info.path;//Store the path of the file for future use
-				console.log('Creating file ' + tempimg);
-				var tempimgfd = info.fd;//Store the file descriptor so I can close the file later
-				temp.open({suffix: '.jpg'}, function(err, info){
-					if (err) throw err;
-					tempdest = info.path;
-					console.log('Creating file ' + tempdest);
-					var tempdestfd =info.fd;
-					request(u, function(error, response, body){//Go get the file at URL u
-						console.log('request is done writing ' + tempimg);
-						if(error || response.statusCode != 200) { //if the status code of the response was not 200,
-										//or request got an error, I know that I didn't get the image I expected
-							fs.closeSync(tempdestfd);//Clean up temp files, I won't be using them.
-							fs.unlinkSync(tempdest);
-							console.log('fs is done deleting ' + tempdest);
-							fs.closeSync(tempimgfd);
-							fs.unlinkSync(tempimg);
-							console.log('fs is done deleting ' + tempimg);
-							res.send(415, 'Sorry, the URL provided '
-									+ 'did not respond with an \'OK\' status code');
-						} else{		
-							im.resize({ //This resizes the file
-								srcPath: tempimg,
-								dstPath: tempdest,
-								width: x,
-								format: 'jpg'
-							}, function(err, stdout, stderr) {
-								if (err) throw err;
-								console.log('ImageMagick is done writing ' + tempdest);
-								fs.close(tempimgfd, function(err){ //Done with tempimg, 
-												//close the file and delete it asynchronously so that 
-												//the response can be sent at the same time
-									if (err) throw err;
-									fs.unlink(tempimg, function(err){
-										if (err) throw err;
-										console.log('fs is done deleting ' + tempimg);
-									});
-								});
-								res.sendfile(tempdest, function(err){//Send the resized file as our response
-									if (err) throw err;
-									fs.closeSync(tempdestfd);//Done with tempdest, close the file and delete it
-									fs.unlinkSync(tempdest);
-									console.log('fs is done deleting ' + tempdest);
-								});
-							});
-						}
-					}).pipe(fs.createWriteStream(tempimg));//piping the request into the temporary file
-				});
-			});
-		}	
-	}*/
 });
 
 http.createServer(app).listen(app.get('port'), function(){
