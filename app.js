8853e6c34b2b70616cd78f45f4ee7a1d927b66d3
Calvin Feder <calvinfeder@gmail.com>
2013-08-28 19:54:47 -0700

    In Memory image resize, no writing to disc.

--- app.js
+++ app.js
@@ -32,10 +32,55 @@ app.get('/resize/:u/:x', function(req, res, next){
 	var x = req.params.x;
 	var parsedurl = url.parse(u);
 	var ext = path.extname(parsedurl.pathname);
-	var tempimg = '';
-	var tempdest = '';
+
 	
-	if(x > 10000 || x <= 0){//only accept dimensions from 1 to 10000 pixels
+	var options = {
+			  hostname: parsedurl.hostname,
+			  port: parsedurl.port,
+			  path: parsedurl.pathname,
+	};
+	
+	if(x > 4000 || x <= 0){//only accept dimensions from 1 to 4000 pixels
+		res.send(406, 'Sorry, the dimensions provided were outside the acceptable range.  '
+				+ 'Please provide dimensions from 1 to 4000 pixels.');
+	} else{
+		if (ext != '.jpg' && ext != '.png' && ext != '.jpeg' && ext != '.bmp') {//only accept these file formats
+			res.send(415, 'Sorry, the URL provided is not an accepted image format. ' 
+					+ 'Please provide a URL with a .jpg, .jpeg, .bmp, or .png extention.');
+		} else{
+			http.get(options, function(response) {
+				if(response.statusCode != 200) { //if the status code of the response was not 200,
+					//or request got an error, I know that I didn't get the image I expected
+					res.send(415, 'Sorry, the URL provided '
+							+ 'did not respond with an \'OK\' status code');
+				} else{
+					var data = '';
+					response.setEncoding('binary');
+					
+					response.on('data', function(chunk) {
+						data += chunk;
+					});
+					
+					response.on('end', function () {
+						im.resize({
+							srcData: data,
+							width: x,
+							format: 'jpg'
+						}, function(err, stdout, stderr) {
+							if (err) throw err;
+							res.writeHead(200, {'Content-Type': 'image/jpg' });
+							res.end(stdout, 'binary');
+						});
+					});
+				}
+			});
+		}
+	}
+	
+	/*
+	 var tempimg = '';
+	 var tempdest = '';
+	 if(x > 10000 || x <= 0){//only accept dimensions from 1 to 10000 pixels
 		res.send(406, 'Sorry, the dimensions provided were outside the acceptable range.  '
 				+ 'Please provide dimensions from 1 to 10000 pixels.');
 	} else{
@@ -95,7 +140,7 @@ app.get('/resize/:u/:x', function(req, res, next){
 				});
 			});
 		}	
-	}
+	}*/
 });
 
 http.createServer(app).listen(app.get('port'), function(){
